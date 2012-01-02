6f21282e759543db6f1699e72fc0a16358df1352
codenamedroid <codenamegit@gmail.com>
2012-01-01 18:58:45 -0600

    Extended Power Menu - Reboot (Danesh M)

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2011 David van Tonder
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -161,9 +162,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         // first: power off
         mItems.add(
-            new SinglePressAction(
-                    com.android.internal.R.drawable.ic_lock_power_off,
-                    R.string.global_action_power_off) {
+            new SinglePressAction(com.android.internal.R.drawable.ic_lock_power_off, R.string.global_action_power_off) {
 
                 public void onPress() {
                     // shutdown by making sure radio and power are handled accordingly.
@@ -178,6 +177,22 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                     return true;
                 }
             });
+        
+        // next: reboot
+        mItems.add(
+            new SinglePressAction(com.android.internal.R.drawable.ic_lock_reboot, R.string.global_action_reboot) {
+                public void onPress() {
+                    ShutdownThread.reboot(mContext, "null", true);
+                }
+                
+                public boolean showDuringKeyguard() {
+                    return true;
+                }
+                
+                public boolean showBeforeProvisioning() {
+                    return true;
+                }
+            });    
 
         // next: airplane mode
         mItems.add(mAirplaneModeOn);
