301275310827341f5d82797737ba5faebd03af2d
codenamedroid <codenamegit@gmail.com>
2012-01-05 23:07:37 -0600

    revert for updated patch

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -25,7 +25,6 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -33,7 +32,6 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.content.res.Configuration;
-import android.database.ContentObserver;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
@@ -398,25 +396,6 @@ public class PhoneStatusBar extends StatusBar {
 	mPowerWidget.setupWidget();
 
         return sb;
-    }
-
-    protected class NavPanelObserver extends ContentObserver {
-        public NavPanelObserver(Handler handler) {
-            super(handler);
-        }
-
-        public void observe() {
-            ContentResolver resolver = mContext.getContentResolver();
-            resolver.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.NAV_BUTTONS),
-                            false, this);
-            onChange(true);
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            prepareNavigationBarView();
-        }
     }
 
     protected WindowManager.LayoutParams getRecentsLayoutParams(LayoutParams layoutParams) {
