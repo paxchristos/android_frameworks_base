4cb1b42167b837608f8ef93eecf75ab97ca0c527
codenamedroid <codenamegit@gmail.com>
2012-02-19 18:08:17 -0600

    add enable crt animations to the config
    Change-Id: I4e1ca7e94a8e79c9a52eae1f66919c2ca91b4baa

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2183,6 +2183,10 @@ public final class Settings {
         @Deprecated
         public static final String WIFI_WATCHDOG_PING_TIMEOUT_MS =
             Secure.WIFI_WATCHDOG_PING_TIMEOUT_MS;
+        
+        public static final String CRT_OFF_ANIMATION = "crt_off_animation";
+
+        public static final String CRT_ON_ANIMATION = "crt_on_animation";
     }
 
     /**
